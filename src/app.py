7851d703f40a6d7ad12d42613d3444b365d6b12c
EsaudTapia <19002144@alumnos.utleon.edu.mx>
2022-04-12 15:27:58 -0500

    se modifica error en class personajes

--- src/app.py
+++ src/app.py
@@ -10,10 +10,10 @@ db=SQLAlchemy(app) #sqlalchemy te paso la config de app
 ma=Marshmallow(app)
 
 class Personaje(db.Model):
-    id= db.column(db.Integer, primary_key=True)
-    nombre= db.column(db.String(70), unique=True)
-    descripcion = db.column(db.String(255))
-    serie =db.column(db.String(100))
+    id= db.Column(db.Integer, primary_key=True)
+    nombre= db.Column(db.String(70), unique=True)
+    descripcion = db.Column(db.String(255))
+    serie =db.Column(db.String(100))
     
     def __init__(self,nombre,descripcion,serie) :
         self.nombre=nombre
